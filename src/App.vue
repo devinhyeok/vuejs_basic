c8c2ac880e0e5bc70bfbab5d5364d8694c2dc6a4
devinhyeok <94621970+devinhyeok@users.noreply.github.com>
2022-11-29 22:36:07 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="black-bg" v-if="isModalOpen == true">
+    <div class="white-bg">
+      <h4>detail</h4>
+      <p>detail</p>
+      <button @click="isModalOpen = false">close</button>
+    </div>
+  </div>
+  <div class="menu">
+    <a v-for="menu in menus" :key="menu">
+      {{ menu }}
+    </a>
+  </div>
+  <div v-for="room, i in rooms" :key="i">
+    <img :src="room.image" class="room-img">
+    <h4>{{ room.title }}</h4>
+    <p>{{ room.price }}원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import roomdata from './assets/room';
 export default {
   name: 'App',
+  data() {
+    return {
+      menus: ['Home', 'Shop', 'About'],
+      rooms: roomdata,
+    }
+  },
+  methods: {
+    increase(i) {
+      this.numberOfReports[i]++
+    },
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -21,6 +45,44 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
 }
 </style>
