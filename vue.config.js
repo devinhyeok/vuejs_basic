cdb63080118b022d4c93fc8db69f61e160581e66
devinhyeok <94621970+devinhyeok@users.noreply.github.com>
2022-11-29 22:44:49 +0900

    gh-pages-init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/vuejs_basic',
+  outputDir: 'docs',
 })
